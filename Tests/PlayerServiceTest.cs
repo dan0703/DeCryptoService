f0b923c8ddf42d565944b69684a20ef3ad56633c
itsue <sujey542003@gmail.com>
2023-12-10 17:43:48 -0600

    tests for methods PlayerService interface

--- Tests/PlayerServiceTest.cs
+++ Tests/PlayerServiceTest.cs
@@ -4,6 +4,7 @@ using Domain;
 using Service;
 using Domain.data;
 using DataAccess;
+using System.Collections.Generic;
 
 namespace Tests
 {
@@ -13,13 +14,45 @@ namespace Tests
         [TestMethod]
         public void Test01_RegisterPlayerSuccesfull()
         {
-            IPlayerServices playerServices = new Implementations();
-            User validUser = new User();
+            IPlayerServices playerService = new Implementations();
+            User validUser = new User
             {
-                
+                name = "Felix",
+                accountNickname = "lixie",
+                birthDay = "24/09/2003"
             };
-            bool result = playerServices.RegisterPlayer(validUser);
+            bool result = playerService.RegisterPlayer(validUser);
             Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test02_GetSimilarNickNamesSuccessfull()
+        {
+            List<String> expected = new List<string>
+            {
+                "user"
+            };
+
+            IPlayerServices playerService = new Implementations();
+            String nickname = "user";
+            
+            List<String> result = playerService.GetSimilarsNickNames(nickname);
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Test02_GetSimilarNickNamesFail()
+        {
+            List<String> expected = new List<string>
+            {
+                "bonie"
+            };
+
+            IPlayerServices playerService = new Implementations();
+            String nickname = "bonie";
+
+            List<String> result = playerService.GetSimilarsNickNames(nickname);
+            CollectionAssert.AreNotEqual(expected, result);
         }
     }
 }
